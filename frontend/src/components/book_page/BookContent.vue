f9265a4017996708461b961b953f851904df5a92
KamilRizatdinov <k.rizatdinov@innopolis.university>
2020-04-19 13:14:14 +0300

    A bit of refactoring

--- frontend/src/components/book_page/BookContent.vue
+++ frontend/src/components/book_page/BookContent.vue
@@ -1,48 +1,49 @@
 <template>
+  <div class="content">
+    <v-container class="pt-9">
+      <v-row>
+        <h1>
+          {{ book.title }}
+        </h1>
+        <v-spacer></v-spacer>
+        <DownloadButton />
+      </v-row>
+    </v-container>
 
-    <div class="content">
-        <v-container class="pt-9">
-            <h1> {{book.title}}
-                <DownloadButton/>
-            </h1>
-        </v-container>
-
-        <v-container style="width: 60%" class="pt-9">
-            <BookPreview class="img" />
-            <h2> Author: {{ book.author.name + ' ' + book.author.surname }}</h2>
-            <h2> Description: </h2>
-            <p> {{book.description}}</p>
-        </v-container>
-
-    </div>
+    <v-container style="width: 60%" class="pt-9">
+      <BookPreview class="img" />
+      <h2>Author: {{ book.author.name + " " + book.author.surname }}</h2>
+      <h2>Description:</h2>
+      <p>{{ book.description }}</p>
+    </v-container>
+  </div>
 </template>
 <script>
-    import DownloadButton from "./DownloadButton";
-    import BookPreview from "./BookPreview";
+import DownloadButton from "./DownloadButton";
+import BookPreview from "./BookPreview";
 
-    export default {
-        name: "Content",
-        components: {BookPreview, DownloadButton},
-        props: ['book']
-    }
+export default {
+  name: "Content",
+  components: { BookPreview, DownloadButton },
+  props: ["book"],
+};
 </script>
 
 <style scoped>
-    .content{
-        width: auto;
-        padding: 20px;
-        overflow: hidden;
-    }
+.content {
+  width: auto;
+  padding: 20px;
+  overflow: hidden;
+}
 
-    .content h3,
-    .content p{
-        display: block;
-        margin: 2px 0 0 0;
-    }
+.content h3,
+.content p {
+  display: block;
+  margin: 2px 0 0 0;
+}
 
-    .img {
-        margin-right: 15px;
-        float: left;
-    }
-
+.img {
+  margin-right: 15px;
+  float: left;
+}
 </style>
--- frontend/src/components/book_page/DownloadButton.vue
+++ frontend/src/components/book_page/DownloadButton.vue
@@ -1,20 +1,16 @@
 <template>
-    <v-btn
-            v-on:click="download()"
-            tale
-            color="green"
-    >
-        <span>DOWNLOAD</span>
-    </v-btn>
+  <v-btn v-on:click="download()" tale color="btn teal lighten-2">
+    <span>DOWNLOAD</span>
+  </v-btn>
 </template>
 
 <script>
-    export default {
-        name: "DownloadButton",
-        methods: {
-            download() {
-                alert("DOWNLOAD STARTED!!! (or not)")
-            }
-        }
-    }
+export default {
+  name: "DownloadButton",
+  methods: {
+    download() {
+      alert("DOWNLOAD STARTED!!! (or not)");
+    },
+  },
+};
 </script>
