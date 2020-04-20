be36601fa54672161cda892b649408afaa6ee296
KamilRizatdinov <k.rizatdinov@innopolis.university>
2020-04-20 17:04:45 +0300

    Add text limitation to book cards

--- frontend/src/components/home/BookTile.vue
+++ frontend/src/components/home/BookTile.vue
@@ -1,27 +1,33 @@
 <template>
-    <v-card
-            elevation="3"
-            class="ma-3 pa-6"
-    >
-        <v-card-title>{{ book.title }}</v-card-title>
-        <v-card-text>{{ book.description }}</v-card-text>
-
-        <v-card-actions>
-            <v-btn v-on:click="read()" text>Read</v-btn>
-        </v-card-actions>
-    </v-card>
+  <v-card elevation="3" class="ma-3 pa-6">
+    <v-card-title>{{ book.title }}</v-card-title>
+    <v-divider></v-divider>
+    <v-card-text style="-webkit-line-clamp: 3">{{ description }}</v-card-text>
+    <v-divider></v-divider>
+    <v-card-actions>
+      <v-btn v-on:click="read()" text>Read</v-btn>
+    </v-card-actions>
+  </v-card>
 </template>
 
 <script>
-    export default {
-        name: 'BookTile',
-        props: ['book'],
-        methods: {
-            read() {
-                this.$router.onReady(() => {
-                    this.$router.push({name: 'book_page', params: {book_id: this.book.id}})
-                })
-            }
-        }
-    }
+export default {
+  name: "BookTile",
+  props: ["book"],
+  methods: {
+    read() {
+      this.$router.onReady(() => {
+        this.$router.push({
+          name: "book_page",
+          params: { book_id: this.book.id },
+        });
+      });
+    },
+  },
+  computed: {
+    description() {
+      return this.book.description.slice(0, 100) + "..";
+    },
+  },
+};
 </script>
